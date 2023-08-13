dc662872534bdeb03007336ced44ab275eb7cc9e
franklindrw <franklindrw@gmail.com>
2023-08-13 15:21:57 -0300

    doc: ajustando a rota do swagger

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('/', app, document);
 
   app.useGlobalPipes(new ValidationPipe()); // adicionando o pipe de validação globalmente
   await app.listen(process.env.PORT || 3000);
