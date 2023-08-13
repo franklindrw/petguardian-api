ca1fb66ced4d442baffe7bb9c9096f9f29a78e7a
franklindrw <franklindrw@gmail.com>
2023-08-13 13:35:35 -0300

    chore: adicionando configuracoes do nest swagger

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import * as dotenv from 'dotenv';
 import { initializeFirebase } from './infra/database/firebase.config';
 import { UsersModule } from './app/users/users.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   dotenv.config();
   initializeFirebase();
   const app = await NestFactory.create(UsersModule);
+
+  // adicionando as configurações do swagger
+  const config = new DocumentBuilder()
+    .setTitle('Pet Guardian API')
+    .setDescription(
+      'API para o projeto Pet Guardian, apresentado como trabalho de conclusão de curso Sistema de Informação - ENIAC 2023',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
